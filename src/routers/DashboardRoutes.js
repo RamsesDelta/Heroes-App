e2dcecebb7ae70c474d880ecdde93c97f799a38b
RamsesDelta <ramses.garcia.cabrera@ibm.com>
2021-07-02 17:23:45 -0500

    init

---
+++ src/routers/DashboardRoutes.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Navbar } from '../components/ui/Navbar'
+
+export const DashboardRoutes = () => {
+    return (
+        <>
+            <Navbar/>
+            <div>
+                <switch>
+                    
+                </switch>
+            </div>
+        </>
+    )
+}
